edfe729e7908c359594cde9fe036ae75f704a976
bada308 <203190@jnu.ac.kr>
2023-08-21 19:41:35 +0900

    feat: 객체(2) 수강

--- bada/section2/src/chapter3.ts
+++ bada/section2/src/chapter3.ts
@@ -1,25 +1,16 @@
 /* 객체 */
 
-let _user:object = {
-    id: 1,
-    name: 'bada',
-}
-
-// _user.id // 에러 발생 ('object' 형식에 'id' 속성이 없습니다.)
-
-
 // 객체 리터럴 타입 정의
-let user:{id:number, name:string} = {
+// id : 옵셔널 프로퍼티
+let user:{id?:number, name:string} = {
     id: 1,
     name: 'bada',
 }
 
-user.id // 가능
-
-let dog:{name: string, color: string} = {
-    name: '멍멍이',
-    color: "brown",
+let config:{
+    readonly apiKey:string;
+} = {
+    apiKey: "MY API KEY",
 }
 
-// typescript는 구조를 기준으로 타입을 정의한다
-// 구조적 타입 시스템
+config.apiKey = 'hacked'; // 에러 발생 (readonly)
